6082ab6e2978eb198bc30bb375a61e90a3c4dad6
Bezara06 <increasescientific@gmail.com>
2026-07-11 18:17:11 +0300

    Add responsive header and footer sections, implement smooth navigation toggle, and enhance styles

---
+++ assets/js/header.js
@@ -0,0 +1,22 @@
+const toggleMenu = document.querySelector('.toggle-menu');
+const nav = document.querySelector('nav');
+
+toggleMenu.addEventListener('click', () => {
+    if (nav.classList.contains('show')) {
+        nav.classList.remove('show');
+    } else {
+        nav.classList.add('show');
+    }
+});
+
+document.addEventListener('click', (event) => {
+    if (!nav.contains(event.target) && !toggleMenu.contains(event.target)) {
+        nav.classList.remove('show');
+    }
+});
+
+document.addEventListener('scroll', () => {
+    if (nav.classList.contains('show')) {
+        nav.classList.remove('show');
+    }
+});
